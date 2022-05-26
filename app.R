24e440c55160eb74e01e5f992a23793c3023798a
Ronan Harrington <ronan_harr@protonmail.com>
2022-05-27 00:03:05 +0100

    Update app.R
    Indented code without vertical alignment

--- app.R
+++ app.R
@@ -20,76 +20,76 @@ ui <- dashboardPage(
   
   # Sidebar
   dashboardSidebar(collapsed = FALSE,
-                   sidebarMenu(
-                     menuItem("Line charts", tabName = "linecharts", 
-                              icon = icon("line-chart"))),
-                   sidebarMenu(
-                     menuItem("Animated graphs", tabName = "animatedgraphs", 
-                              icon = icon("spinner"))),
-                   sidebarMenu(
-                     menuItem("Source code", icon = icon("file-code-o"), 
-                              href = "https://github.com/rnnh/covidgraphics"))
-                   ),
+    sidebarMenu(
+      menuItem("Line charts", tabName = "linecharts", 
+        icon = icon("line-chart"))),
+    sidebarMenu(
+      menuItem("Animated graphs", tabName = "animatedgraphs", 
+        icon = icon("spinner"))),
+    sidebarMenu(
+      menuItem("Source code", icon = icon("file-code-o"), 
+        href = "https://github.com/rnnh/covidgraphics"))
+  ),
   
   # Body
   dashboardBody(
     tabItems(
       tabItem(tabName = "linecharts",
-              fluidRow(
-                box(title = "COVID-19 graph controls",
-                    "The graphs below will update automatically according to
+        fluidRow(
+          box(title = "COVID-19 graph controls",
+            "The graphs below will update automatically according to
                     these inputs.",
-                    "Enter the 3 letter ISO code for a country to display its
+            "Enter the 3 letter ISO code for a country to display its
                     COVID-19 data.",
-                    "Country codes must be in capitals, and separated with 
+            "Country codes must be in capitals, and separated with 
                     spaces.",
-                    "Use the slider to select the date range to display.",
-                    textInput("countries_lc", "3 letter ISO country codes",
-                              "USA BRA RUS PER ITA"),
-                    sliderInput(inputId = "date_lc", label = "Date range",
-                                min = min(covid19_df$date),
-                                max = max(covid19_df$date), 
-                                value = c(max(covid19_df$date) - 180,
-                                          max(covid19_df$date)), step = 1))
-                ),
-              fluidRow(
-                box(plotOutput("lp_case_time_pc")),
-                box(plotOutput("lp_vaccines_time_pc"))
-              ),
-              fluidRow(
-                box(plotOutput("lp_hosp_time_pc")),
-                box(plotOutput("lp_death_time_pc"))
-              )),
+            "Use the slider to select the date range to display.",
+            textInput("countries_lc", "3 letter ISO country codes",
+              "USA BRA RUS PER ITA"),
+            sliderInput(inputId = "date_lc", label = "Date range",
+              min = min(covid19_df$date),
+              max = max(covid19_df$date), 
+              value = c(max(covid19_df$date) - 180,
+                max(covid19_df$date)), step = 1))
+        ),
+        fluidRow(
+          box(plotOutput("lp_case_time_pc")),
+          box(plotOutput("lp_vaccines_time_pc"))
+        ),
+        fluidRow(
+          box(plotOutput("lp_hosp_time_pc")),
+          box(plotOutput("lp_death_time_pc"))
+        )),
       
       tabItem(tabName = "animatedgraphs",
-              fluidRow(
-                box(title = "COVID-19 animated graph controls",
-                    'This animated graph will update according to these inputs 
+        fluidRow(
+          box(title = "COVID-19 animated graph controls",
+            'This animated graph will update according to these inputs 
                     when the "Create animated graph" button below is pressed.',
-                    "This graph will take a few moments to render, please be 
+            "This graph will take a few moments to render, please be 
                     patient.",
-                    "Enter the 3 letter ISO code for a country to display its
+            "Enter the 3 letter ISO code for a country to display its
                     COVID-19 data.",
-                    "Country codes must be in capitals, and separated with 
+            "Country codes must be in capitals, and separated with 
                     spaces.",
-                    "Use the slider to select the date range to display.",
-                    sliderInput(inputId = "date", label = "Date range",
-                                min = min(covid19_df$date),
-                                max = max(covid19_df$date), 
-                                value = c(max(covid19_df$date) - 180,
-                                          max(covid19_df$date)), step = 1),
-                    textInput("countries", "Select countries (ISO codes)",
-                              "USA BRA RUS PER ITA"),
-                    actionButton('goPlot', 'Create animated graph (please wait)')
-                )),
-              fluidRow(
-                box(title = "Animated Scatter Plot",
-                    plotOutput("gifPlot", height = 600)
-                    ))
-              ) # tabItem()
-      ) # tabItems()
-      ) # dashboardBody()
-  ) # dashboardPage()
+            "Use the slider to select the date range to display.",
+            sliderInput(inputId = "date", label = "Date range",
+              min = min(covid19_df$date),
+              max = max(covid19_df$date), 
+              value = c(max(covid19_df$date) - 180,
+                max(covid19_df$date)), step = 1),
+            textInput("countries", "Select countries (ISO codes)",
+              "USA BRA RUS PER ITA"),
+            actionButton('goPlot', 'Create animated graph (please wait)')
+          )),
+        fluidRow(
+          box(title = "Animated Scatter Plot",
+            plotOutput("gifPlot", height = 600)
+          ))
+      ) # tabItem()
+    ) # tabItems()
+  ) # dashboardBody()
+) # dashboardPage()
 
 
 # Define shiny server function
@@ -117,13 +117,13 @@ server <- function(input, output) {
   output$lp_death_time_pc <- renderPlot({
     covid19_timeframe.df <- covid19_reactive()
     ggplot(covid19_timeframe.df, aes(x = date, y = (deaths / population) * 100000,
-                                     color = administrative_area_level_1)) +
+      color = administrative_area_level_1)) +
       geom_line() + 
       geom_point() +
       labs(title = "COVID-19: Deaths per 100,000 vs. Time",
-           x = "Time",
-           y = "Deaths per 100,000 Population",
-           color = "Country") +
+        x = "Time",
+        y = "Deaths per 100,000 Population",
+        color = "Country") +
       theme_bw() +
       theme(text = element_text(size = 16))})
   
@@ -137,9 +137,9 @@ server <- function(input, output) {
       geom_line() + 
       geom_point() +
       labs(title = "COVID-19: Cases per 100,000 vs. Time",
-           x = "Time",
-           y = "Cases per 100,000 Population",
-           color = "Country") +
+        x = "Time",
+        y = "Cases per 100,000 Population",
+        color = "Country") +
       theme_bw() +
       theme(text = element_text(size = 16))})
   
@@ -153,9 +153,9 @@ server <- function(input, output) {
       geom_line() + 
       geom_point() +
       labs(title = "COVID-19: Vaccinations per 100,000 vs. Time",
-           x = "Time",
-           y = "Vaccinations per 100,000 Population",
-           color = "Country") +
+        x = "Time",
+        y = "Vaccinations per 100,000 Population",
+        color = "Country") +
       theme_bw() +
       theme(text = element_text(size = 16))})
   
@@ -169,12 +169,12 @@ server <- function(input, output) {
       geom_line() + 
       geom_point() +
       labs(title = "COVID-19: Hospitalisations per 100,000 vs. Time",
-           x = "Time",
-           y = "Hospitalisations per 100,000 Population",
-           color = "Country") +
+        x = "Time",
+        y = "Hospitalisations per 100,000 Population",
+        color = "Country") +
       theme_bw() +
       theme(text = element_text(size = 16))})
-
+  
   # Animated plot: Scatter plot
   output$gifPlot <- renderImage({
     covid19_timeframe.df <- covid19_button()
@@ -187,15 +187,15 @@ server <- function(input, output) {
     detach(covid19_timeframe.df)
     
     p <- ggplot(covid19_timeframe.df, aes(x, y, size = population,
-                                          color = administrative_area_level_1)) +
+      color = administrative_area_level_1)) +
       geom_point() +
       theme_bw() +
       theme(text = element_text(size = 16)) +
       labs(title = "COVID-19: Cases vs. Deaths per 100,000 Population",
-           subtitle = "Points scaled to population. Date: {frame_time}",
-           x = "Deaths per 100,000 Population",
-           y = "Confirmed cases per 100,000 Population",
-           color = "Country") +
+        subtitle = "Points scaled to population. Date: {frame_time}",
+        x = "Deaths per 100,000 Population",
+        y = "Confirmed cases per 100,000 Population",
+        color = "Country") +
       guides(size = FALSE) +
       transition_time(date) +
       ease_aes("linear")
@@ -208,10 +208,10 @@ server <- function(input, output) {
     
     # List containing the GIF filename
     list(src = "outfile.gif",
-         contentType = "image/gif",
-         alt = "COVID-19: Cases vs Deaths"
+      contentType = "image/gif",
+      alt = "COVID-19: Cases vs Deaths"
     )}, deleteFile = TRUE)
-
+  
 }
 
 # Run the application
